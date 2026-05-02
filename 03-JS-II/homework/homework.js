43f033a266e6a9391dd6d511e6a0e20ec8d2ec46
Herrera Daniela <herreradr93@gmail.com>
2026-05-02 17:30:28 -0300

    homework3 terminado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -167,7 +167,7 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si no se cumplen ninguna de las condiciones anteriores, retogit pushrnar false. 
 
   if (num1 === 0 || num2 === 0 || num3 ===0) {
     return "Error";
